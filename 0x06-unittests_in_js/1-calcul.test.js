8f938231335687a5c83fd3c7c4aa374c7c44e7b5
Emedo586 <emedopeace99@gmail.com>
2024-06-06 10:03:32 +0000

    CORRECTIONS

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -37,3 +37,46 @@ describe('calculateNumber', function () {
       assert.equal(calculateNumber('SUBTRACT', 2, 4.5), -3);
     });
   });
+
+  describe('SUBTRACT second round', function () {
+    it('should return 3', function () {
+      assert.equal(calculateNumber('SUBTRACT', 4.5, 2), 3);
+    });
+  });
+
+  describe('SUBTRACT both round', function () {
+    it('should return -4', function () {
+      assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    });
+  });
+
+  describe('DIVIDE no round', function () {
+    it('should return 2', function () {
+      assert.equal(calculateNumber('DIVIDE', 8, 4), 2);
+    });
+  });
+
+  describe('DIVIDE first round', function () {
+    it('should return 5', function () {
+      assert.equal(calculateNumber('DIVIDE', 9.5, 2), 5);
+    });
+  });
+
+  describe('DIVIDE second round', function () {
+    it('should return 0.2', function () {
+      assert.equal(calculateNumber('DIVIDE', 2, 9.5), 0.2);
+    });
+  });
+
+  describe('DIVIDE both round', function () {
+    it('should return 0.2', function () {
+      assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    });
+  });
+
+  describe('DIVIDE Error', function () {
+    it('should return Error', function () {
+      assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    });
+  });
+});
